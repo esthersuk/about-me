4e4f4b9001a5f78cbb60c967ad369af52996d697
Esther Suk <sestherr@gmail.com>
2017-06-06 15:04:44 -0700

    changes

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+
+var response = confirm("Are you ready to play?");
+if (response){
+  console.log("Let's do this!");
+} else {
+  console.log("Bye Felicia!")
+}
+var response = prompt("Do ")
